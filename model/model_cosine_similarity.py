8f562359eb52a961df49ec3bc3ab082f94b82389
stu mals <stuartmals23@gmail.com>
2022-11-19 15:35:16 -0500

    updated recommender to run faster by using sorted indicies from numpy argsort

--- model/model_cosine_similarity.py
+++ model/model_cosine_similarity.py
@@ -10,19 +10,16 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 df_raw = pd.read_csv('tracks_features.csv')
 df_raw['artists'] = df_raw.loc[:,'artists'].str.strip("[]'").str.split(',')
 
-
 class SongRecommender():
     
     def __init__(self, df_raw, song_name, song_id=None, client_id=None, client_secret=None):
 
+        assert song_name in list(df_raw['name']), 'Song not in tracks_features dataset'
+            
         if song_id == None:
-            print(song_name)
             self.song_info = df_raw[df_raw['name']==song_name].iloc[0,:]
-            print('song name executed')
         else:
-            print(song_id)
             self.song_info = df_raw[df_raw['id']==song_id]
-            print('song id executed')
             
         self.df_raw = df_raw
         
@@ -37,6 +34,7 @@ class SongRecommender():
         x_norm = np.linalg.norm(x, axis=1)
         y_norm = np.linalg.norm(y, axis=1)
         norm_prod = x_norm * y_norm
+        
         return dot_prods/norm_prod
 
     def recommender(self, num_songs=10):
@@ -54,13 +52,15 @@ class SongRecommender():
         cosine_sim = self.cosine_sim_calc(x, y)
         songs['similarity_score'] = cosine_sim
 
-        return songs.sort_values(by='similarity_score', ascending=False).iloc[:num_songs, :]
-        
+        sorted_ids = np.argsort(cosine_sim)[::-1][:num_songs]
+
+        #return songs.sort_values(by='similarity_score', ascending=False).iloc[:num_songs, :]
+        return songs.loc[sorted_ids, :]
 
 def artist_network(df_raw, playlist, limit=20, num_songs=10, num_related=5):
     '''
     Build artist network based artist of first song in playlist
-    
+
     limit: number of times to generate of playlist from arists being generated in network
     num_songs: number of songs generated from each additional playlist
     num_related: number of additional artists to add to network
@@ -85,7 +85,6 @@ def artist_network(df_raw, playlist, limit=20, num_songs=10, num_related=5):
         song_names_new = list(new_playlist['name'])
 
         artist_main = artists_new[0][0]
-        print(artist_main)
         for artist_list in artists_new:
             for a in artist_list:
                 graph.add_node(a)
