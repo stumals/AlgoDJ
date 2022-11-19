e8945442321a40a38a1744d0ff71d3e2f0951dc2
stu mals <stuartmals23@gmail.com>
2022-11-19 14:35:47 -0500

    added artist_network function to model

--- model/model_cosine_similarity.py
+++ model/model_cosine_similarity.py
@@ -1,22 +1,29 @@
-#%%
 import pandas as pd
 import numpy as np
 import networkx as nx
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 pd.options.mode.chained_assignment = None
-
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-#%%
+
 df_raw = pd.read_csv('tracks_features.csv')
 df_raw['artists'] = df_raw.loc[:,'artists'].str.strip("[]'").str.split(',')
-#%%
+
+
 class SongRecommender():
     
-    def __init__(self, df_raw, song_name, client_id=None, client_secret=None):
+    def __init__(self, df_raw, song_name, song_id=None, client_id=None, client_secret=None):
 
-        self.song_info = df_raw[df_raw['name']==song_name].iloc[0,:]
+        if song_id == None:
+            print(song_name)
+            self.song_info = df_raw[df_raw['name']==song_name].iloc[0,:]
+            print('song name executed')
+        else:
+            print(song_id)
+            self.song_info = df_raw[df_raw['id']==song_id]
+            print('song id executed')
+            
         self.df_raw = df_raw
         
     def cosine_sim_calc(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
@@ -47,6 +54,48 @@ class SongRecommender():
         cosine_sim = self.cosine_sim_calc(x, y)
         songs['similarity_score'] = cosine_sim
 
-        # returns dataframe with song name, artists, and similarity score
-        return songs.sort_values(by='similarity_score', ascending=False).iloc[:num_songs, :]    
-#%%
+        return songs.sort_values(by='similarity_score', ascending=False).iloc[:num_songs, :]
+        
+
+def artist_network(df_raw, playlist, limit=20, num_songs=10, num_related=5):
+    '''
+    Build artist network based artist of first song in playlist
+    
+    limit: number of times to generate of playlist from arists being generated in network
+    num_songs: number of songs generated from each additional playlist
+    num_related: number of additional artists to add to network
+
+    returns networkx graph
+    '''
+
+    graph = nx.Graph()
+    artists_base = list(playlist['artists'])
+    song_ids_base = list(playlist['id'])
+    song_names_base = list(playlist['name'])
+    artist_main = artists_base[0][0]
+    for artist_list in artists_base:
+        for a in artist_list:
+            graph.add_node(a)
+            graph.add_edge(artist_main, a)
+    i = 1
+    while i < limit:
+        new_playlist = SongRecommender(df_raw, song_names_base[i], song_id=song_ids_base[i]).recommender(num_songs=num_songs)
+        artists_new = list(new_playlist['artists'])
+        song_ids_new = list(new_playlist['id'])
+        song_names_new = list(new_playlist['name'])
+
+        artist_main = artists_new[0][0]
+        print(artist_main)
+        for artist_list in artists_new:
+            for a in artist_list:
+                graph.add_node(a)
+                graph.add_edge(artist_main, a)
+
+        for j in range(num_related):
+            song_ids_base.append(song_ids_new[j])
+            song_names_base.append(song_names_new[j])
+        i += 1
+        
+    graph.remove_edges_from(nx.selfloop_edges(graph))
+
+    return graph
