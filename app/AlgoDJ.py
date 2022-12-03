995d522ab7160ded9ed3a598f8b02e3526d574f8
grantchan39 <rchan39@DESKTOP-VM2B39P.localdomain>
2022-12-02 23:29:38 -0600

    refreshable playlist

--- app/AlgoDJ.py
+++ app/AlgoDJ.py
@@ -1,28 +1,155 @@
-from os import path
-from dash import Dash, html, dcc
-from dash.dependencies import Output, Input
-import plotly.express as px
-import dash_bootstrap_components as dbc 
-import pandas as pd
-from viz.ADJ_layout1 import ADJ_Layout
-# from viz.ADJ_layout1 import init_input_layout1_callbacks
+import dash
+import pickle
+from dash import dcc, html
+from dash.dependencies import Output, Input, State
+from dash.exceptions import PreventUpdate
+from pathlib import Path
+import dash_bootstrap_components as dbc
+from viz.header import Header
+from viz.user_input_row1 import UserInput1
+from viz.user_input_row3 import UserInput3
+from viz.wordcloudviz import WordCloudViz
+from viz.timelineviz import TimeLineViz
+from viz.network_graph import NetworkGraph
+from viz.user_input_row1 import *
+from viz.user_input_row3 import *
+from model.track_network import TrackNetwork
 
-app = Dash(__name__,external_stylesheets=[dbc.themes.DARKLY], meta_tags=[{'name': 'viewport', 'content': 'width=device-width,initial-scale=1.0'}])
+with open(Path(Path.cwd(), "data","pickle_data", "songsample.pickle"), "rb") as f:
+    user_input = pickle.load(f)
+
+track_network = TrackNetwork(limit=10, num_songs=5, num_related=3)
+recs = track_network.get_recommendations(user_input)
+network = track_network.build_network(recs, "track")
+playlist = track_network.get_playlist(recs, num_songs=10)
+networkgraph = NetworkGraph(user_input).networkgraphapp
+wordcloud = WordCloudViz(user_input).wordcloudapp
+timeline = TimeLineViz(user_input).timelineapp
+
+app = dash.Dash(__name__ ,external_stylesheets=[dbc.themes.DARKLY], meta_tags=[{'name': 'viewport', 'content': 'width=device-width,initial-scale=1.0'}])
 server = app.server
 
+UserInput2 = html.Div([
 
+    dbc.Row([
+        dbc.Col([
+            dbc.Label("Title"),
+            dbc.Input(id = "title", placeholder="Valid input...", valid=True, className="mb-3"),
+        ],width={"size":9,"order":1}),
+        dbc.Col([
+            dbc.Label("Song sample"),
+            dbc.Button(id ="song-sample",
+            children = "Upload",
+            color="primary",
+            href="/",
+            external_link=True,
+            ),
+        ],width={"size":3,"order":2})
+    ]),
 
-app.layout = dbc.Container([
-    html.Div([ADJ_Layout])
+    dbc.Row([
+        dcc.Upload(html.A('Upload File')),
+
+        html.Hr(),
+
+        dcc.Upload([
+        'Drag and Drop or ',
+        html.A('Select a File')
+        ], style={
+        'width': '100%',
+        'height': '60px',
+        'lineHeight': '60px',
+        'borderWidth': '1px',
+        'borderStyle': 'dashed',
+        'borderRadius': '5px',
+        'textAlign': 'center'
+    })
+    ]),
+
+    html.Br(),
+    dcc.Store(id="store-data2", data=[], storage_type="memory")
+
 ])
 
-# init_input_layout1_callbacks(app)
+app.layout = dbc.Container([
+    html.Div([dbc.Container([
+dbc.Row([Header]),
+dbc.Row([
+    dbc.Col([dbc.Row([dbc.Row([UserInput1, UserInput2, UserInput3])]), dbc.Row([wordcloud])]),
+    dbc.Col([dbc.Row([networkgraph]), dbc.Row([timeline])])
+    ]),
+dcc.Store(id="store-data", data=[], storage_type="memory"),
+html.Div(id="hidden", style={"display": "none"})
+])])
+])
 
- 
+
+@app.callback(
+    Output("hidden", "children"),
+    Input("song-sample", "n_clicks"),
+    State("title", "value")
+)
+def update_song(n_clicks, ui):
+    if ui is not None:
+        with open(Path(Path.cwd(), "data","pickle_data", "songsample.pickle"), "wb") as f:
+            pickle.dump(ui, f) 
+        networkgraph = NetworkGraph(ui).networkgraphapp
+        wordcloud = WordCloudViz(ui).wordcloudapp
+        timeline = TimeLineViz(ui).timelineapp
+        UserInput2 = html.Div([
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.Label("Title"),
+                            dbc.Input(id = "title", placeholder=ui, valid=True, className="mb-3"),
+                        ],width={"size":9,"order":1}),
+                        dbc.Col([
+                            dbc.Label("Song sample"),
+                            dbc.Button(id ="song-sample",
+                            children = "Upload",
+                            color="primary",
+                            href="/",
+                            external_link=True,
+                            ),
+                        ],width={"size":3,"order":2})
+                    ]),
+
+                    dbc.Row([
+                        dcc.Upload(html.A('Upload File')),
+
+                        html.Hr(),
+
+                        dcc.Upload([
+                        'Drag and Drop or ',
+                        html.A('Select a File')
+                        ], style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center'
+                    })
+                    ]),
+                    html.Br(),
+                    dcc.Store(id="store-data2", data=[], storage_type="memory")
+                ])
+        app.layout = dbc.Container([
+            html.Div([dbc.Container([
+        dbc.Row([Header]),
+        dbc.Row([
+            dbc.Col([dbc.Row([dbc.Row([UserInput1, UserInput2, UserInput3])]), dbc.Row([wordcloud])]),
+            dbc.Col([dbc.Row([networkgraph]), dbc.Row([timeline])])
+            ]),
+        dcc.Store(id="store-data", data=[], storage_type="memory"),
+        html.Div(id="hidden", style={"display": "none"})
+        ])])
+        ])
+        return ui
+    return ui
 
 if __name__ == '__main__':
     app.run_server(debug=True, threaded=False, use_reloader=False, port=8050)
-    # app.run_server(debug=True)
     
     
 
--- viz/ADJ_layout1.py
+++ viz/ADJ_layout1.py
@@ -1,4 +1,3 @@
-from os import path
 import dash
 from dash import dcc, html
 from dash.dependencies import Output, Input, State
@@ -7,17 +6,17 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 from viz.header import Header
 from viz.user_input_row1 import UserInput1
-from viz.user_input_row2 import UserInput2
+# from viz.user_input_row2 import UserInput2
 from viz.user_input_row3 import UserInput3
 from viz.wordcloudviz import WordCloudViz
 from viz.timelineviz import TimeLineViz
 from viz.network_graph import NetworkGraph
 from viz.user_input_row1 import *
-from viz.user_input_row2 import *
+# from viz.user_input_row2 import *
 from viz.user_input_row3 import *
 from model.track_network import TrackNetwork
 
-user_input = "Down on the Farm"
+user_input = "Calm Like a Bomb"
 
 track_network = TrackNetwork(limit=10, num_songs=5, num_related=3)
 recs = track_network.get_recommendations(user_input)
@@ -27,26 +26,88 @@ networkgraph = NetworkGraph(user_input).networkgraphapp
 wordcloud = WordCloudViz(user_input).wordcloudapp
 timeline = TimeLineViz(user_input).timelineapp
 
-
 app = dash.Dash(__name__ ,external_stylesheets=[dbc.themes.DARKLY], meta_tags=[{'name': 'viewport', 'content': 'width=device-width,initial-scale=1.0'}])
 
+UserInput2 = html.Div([
+
+    dbc.Row([
+        dbc.Col([
+            dbc.Label("Title"),
+            dbc.Input(id = "title", placeholder="Valid input...", valid=True, className="mb-3"),
+        ],width={"size":9,"order":1}),
+        dbc.Col([
+            dbc.Label("Song sample"),
+            dbc.Button(id ="song-sample",
+            children = "Upload",
+            # href="/static/data_file.txt",
+            # download="my_data.txt",
+            # external_link=True,
+            color="primary",
+            ),
+        ],width={"size":3,"order":2})
+    ]),
+
+    dbc.Row([
+        dcc.Upload(html.A('Upload File')),
+
+        html.Hr(),
+
+        dcc.Upload([
+        'Drag and Drop or ',
+        html.A('Select a File')
+        ], style={
+        'width': '100%',
+        'height': '60px',
+        'lineHeight': '60px',
+        'borderWidth': '1px',
+        'borderStyle': 'dashed',
+        'borderRadius': '5px',
+        'textAlign': 'center'
+    })
+    ]),
+
+    html.Br(),
+    dcc.Store(id="store-data2", data=[], storage_type="memory")
+
+])
 
 ADJ_Layout = dbc.Container([
-    dbc.Row([Header]),
-    dbc.Row([
-        dbc.Col([dbc.Row([dbc.Row([UserInput1, UserInput2, UserInput3])]), dbc.Row([wordcloud])]),
-        dbc.Col([dbc.Row([networkgraph]), dbc.Row([timeline])])
-        ]),
-    # dbc.Row([
-    #     dbc.Col([
-    #         dbc.Row([UserInput1, UserInput2, UserInput3, wordcloud]),
-    #         # dbc.Row([playlist]),
-    #         ]),
-    #     dbc.Col([networkgraph, timeline]),
-    #     # dbc.Col([]),
-    #     ]),
-    dcc.Store(id="store-data", data=[], storage_type="memory")
+dbc.Row([Header]),
+dbc.Row([
+    dbc.Col([dbc.Row([dbc.Row([UserInput1, UserInput2, UserInput3])]), dbc.Row([wordcloud], id="word-cloud")]),
+    dbc.Col([dbc.Row([networkgraph], id="network-graph"), dbc.Row([timeline], id="time-line")])
+    ]),
+dcc.Store(id="store-data", data=[], storage_type="memory")
 ])
+
+@app.callback(
+    # Output("first_output_1", "children"),
+    Output("network-graph", "figure"),
+    Output("word-cloud", "figure"),
+    Output("time-line", "figure"),
+    Input("song-sample", "n_clicks")
+)
+def change_text(user_input):
+    # recs = track_network.get_recommendations(user_input)
+    # network = track_network.build_network(recs, "track")
+    # playlist = track_network.get_playlist(recs, num_songs=10)
+    networkgraph = NetworkGraph(user_input).networkgraphapp
+    wordcloud = WordCloudViz(user_input).wordcloudapp
+    timeline = TimeLineViz(user_input).timelineapp
+    return networkgraph, wordcloud, timeline
+    
+    # ADJ_Layout = dbc.Container([
+    # dbc.Row([Header]),
+    # dbc.Row([
+    #     dbc.Col([dbc.Row([dbc.Row([UserInput1, UserInput2, UserInput3])]), dbc.Row([wordcloud])]),
+    #     dbc.Col([dbc.Row([networkgraph]), dbc.Row([timeline])])
+    #     ]),
+    # dcc.Store(id="store-data", data=[], storage_type="memory")
+    # ])
+    # return ADJ_Layout
+
+
+
 
 # def init_input_layout1_callbacks(app):
 
